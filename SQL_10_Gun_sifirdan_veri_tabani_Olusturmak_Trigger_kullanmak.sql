00fe53f54a4a7a4f3a2b9c491e1ff036fd5e662c
CuneytAtesman <105663657+CuneytAtesman@users.noreply.github.com>
2024-09-04 04:01:51 +0300

    Create SQL_10_Gun_sifirdan_veri_tabani_Olusturmak_Trigger_kullanmak.sql

---
+++ SQL_10_Gun_sifirdan_veri_tabani_Olusturmak_Trigger_kullanmak.sql
@@ -0,0 +1,25 @@
+
+
+
+-- SIFIRDAN YENÝ VERÝ TABANI OLUÞTURMAK
+-- VERÝ TABANI KOLONLARINI PLANLAMAK.
+
+-- TRIGGER
+
+/* Silinen Öðrenciyi Mezun Oldu Tablosuna Kaydetmek veya sipariþ alýndýðýnda stokdan düþmek.
+veya iadeleri tekrar tabloya Triggerle otomatik eklemek*/
+
+-- Kütüphane - Normalizasyona göre Tablo Yapýsý Çýkartmak.
+
+-- tbl_ogrenci / ogrenciID,ogrenciAd,ogrenciSoyad,cinsiyet,DogumTarihi,sinif,puan,aktif
+-- tbl_kitap  / kitapID,adi,isbnNo,yazarID,turID,yayineviID
+
+-- tbl_yazar  / yazarID,adi,
+-- tbl_tur    /turID,turAdi,
+-- tbl_yayinevi / yayineviID,adi,adres,telefon,email
+
+-- Bir sonraki derste (11 Gun) devam edilecek.
+
+
+
+
